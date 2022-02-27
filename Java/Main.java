3e17369d7f5b1485ec27392c011f59b5fe0d500b
kgarcia5454 <kgarcia5454@gmail.com>
2022-02-27 13:39:19 -0700

    Uploading text version of game
    Includes Disrespectful mode. Disrespectful mode allow you to move opponent tokens.
    Next task is to re-format code so all the code is not on main.

--- Java/Main.java
+++ Java/Main.java
@@ -20,9 +20,10 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("\n Disrespectful 4 in a row! \n" );
-        System.out.println("1. Play");
-        System.out.println("2. Quit");
+        System.out.println("\nDisrespectful 4 in a row! \n" );
+        System.out.println("1.Play Classic");
+        System.out.println("2.Quit");
+        System.out.println("3.Disrespectful Mode");
 
         int GameMode = scan.nextInt();
 
@@ -33,7 +34,7 @@ public class Main {
                 printRow(row);
             }
             
-            System.out.print("\nWhere do you wanna drop?");
+            System.out.print("\nWhere do you wanna drop? ");
 
             int Dropped = scan.nextInt();
 
@@ -43,23 +44,91 @@ public class Main {
                 if(A_Row != 9){
                     GameOver = WinCheck(GameBoardArray, Dropped,player,A_Row);
                 }   
+
+                if(player == 1){
+                    player++;
+                }else{
+                    player--;
+                }
             }
-            if(player == 1){
-                player++;
-            }else{
-                player--;
-            }
+            
 
             if(GameOver == 1){
                 GameMode=2;
             }
             
         }
+
+    
         if(GameMode == 2){
             scan.close();
             System.exit(0);
         }
-        
+
+        while(GameMode == 3){
+                //printing out the board
+                System.out.println("1 2 3 4 5 6 7");
+                for(int[] row : GameBoardArray){
+                    printRow(row);
+                }
+                System.out.println("Dropping or Moving");
+                System.out.println("1. Dropping");
+                System.out.println("2. Moving");
+
+                int type = scan.nextInt();
+
+                if(type==1) {
+                    
+                    System.out.print("Where do you wanna drop? ");
+                    int Dropped = scan.nextInt();
+
+                    if(Dropped>=1 && Dropped<=7){
+
+                        int A_Row = tokenCheck(GameBoardArray, Dropped,player);
+
+                        if(A_Row != 9){
+                            GameOver = WinCheck(GameBoardArray, Dropped,player,A_Row);
+                        }   
+
+                        if(player == 1){
+                            player++;
+                        }else{
+                            player--;
+                        }
+                    }
+                }
+
+                if(type==2){
+                    System.out.print("\nWhich opponent piece do you want to move (Top Layer Only)");
+                    int move = scan.nextInt();
+
+                    if(move>=1 && move <=7){
+
+                        int A_row = OpponentTokenCheck(GameBoardArray,move,player);
+                        
+                        if(A_row !=9){
+
+                            if(player == 1){
+                                player++;
+                            }else{
+                                player--;
+                            }
+
+                            System.out.println("Where do you want to move piece");
+
+                            int new_col = scan.nextInt();
+
+                            int B_Row = tokenCheck(GameBoardArray, new_col,player);
+
+                            if(B_Row != 9){
+                                GameOver = WinCheck(GameBoardArray, new_col,player,B_Row);
+                            }   
+                        }                    
+                    }
+
+                }
+
+        }
     }
 
     public static void printRow(int[] row) {
@@ -265,6 +334,54 @@ public class Main {
         }
 
         return Winner;
+
+    }
+
+    public static int OpponentTokenCheck(int [][] array, int selected, int play_Num){
+        int tested =0;
+        int chk_row = 0;
+        int Opponent = 9;
+
+
+        //Searches for Opponents token on highest layer
+        while(tested==0){
+            //Checks player
+            if(play_Num == 1 ){
+                if(array[chk_row][selected-1]==2){
+                    Opponent = chk_row;   
+                    tested=1;
+                }else{
+                    if(chk_row < 5){
+                        chk_row++;
+                    }else{
+
+                        tested=1;
+                    }
+                }
+            }
+
+            if(play_Num == 2 ){
+                if(array[chk_row][selected-1]==1){
+                    Opponent = chk_row;   
+                    tested=1;
+                }else{
+                    if(chk_row < 5){
+                        chk_row++;
+                    }else{
+                        tested=1;
+                    }
+                }
+            }
+        }
+
+        if(Opponent==9){
+            System.out.println("no token found");
+        }else{
+            System.out.println("token found at "+ selected + "," + Opponent);
+            array[Opponent][selected-1] = 0;
+        }
+
+        return Opponent;
 
     }
 }
